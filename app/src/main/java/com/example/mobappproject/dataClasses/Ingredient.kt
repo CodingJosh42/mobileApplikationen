95a2cb28914aa505e17e0735605b18a13396a7ff
Joshua Martin <joshuamartin997@googlemail.com>
2020-12-26 16:39:18 +0100

    Ingredients einzigartig in der liste unabhängig von groß oder kleinschreibung

--- app/src/main/java/com/example/mobappproject/dataClasses/Ingredient.kt
+++ app/src/main/java/com/example/mobappproject/dataClasses/Ingredient.kt
@@ -1,3 +1,16 @@
 package com.example.mobappproject.dataClasses
 
-data class Ingredient(val name: String)
+import java.util.*
+
+
+data class Ingredient(val name: String) {
+    override fun equals(other: Any?): Boolean {
+        if(other != null && other is Ingredient)
+            return other.name.equals(name, ignoreCase = true)
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+}
