56303bfd1bc14d74468c67a47e00b98ad0de8d9e
Joshua Martin <joshuamartin997googlemail.com>
2020-12-29 20:32:03 +0100

    Ingredients können durch swipen entfernt werden

--- app/src/main/java/com/example/mobappproject/recycleViewIngredients/RecyclerAdapter.kt
+++ app/src/main/java/com/example/mobappproject/recycleViewIngredients/RecyclerAdapter.kt
@@ -7,27 +7,37 @@ import com.example.mobappproject.dataClasses.Ingredient
 
 class RecyclerAdapter(private val list: ArrayList<Ingredient>) : RecyclerView.Adapter<IngredientHolder>() {
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientHolder {
         val inflater = LayoutInflater.from(parent.context)
         return IngredientHolder(inflater, parent)
     }
 
+    fun removeItem(position: Int){
+        list.removeAt(position)
+        if(position != 0) {
+            notifyItemRangeChanged(position, list.size)
+        } else {
+            notifyDataSetChanged()
+        }
+    }
     override fun onBindViewHolder(holder: IngredientHolder, position: Int) {
         val ingredient: Ingredient = list[position]
         holder.bind(ingredient)
 
         val button = holder.getButton()
         button?.setOnClickListener {
-            list.removeAt(position)
-            if(position != 0)
-                notifyItemRangeChanged(position, list.size)
-            else
-                notifyDataSetChanged()
+            removeItem(position)
         }
     }
 
     override fun getItemCount(): Int {
         return  list.size
     }
+
+    fun getList(): ArrayList<Ingredient>  {
+        return list
+    }
+
 
 }
