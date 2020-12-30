70904663cca07b960d29895b5e20e839a1c54044
Joshua Martin <joshuamartin@googlemail.com>
2020-12-30 21:46:32 +0100

    IngredientList, DBIngredients werden in Ingredients umgewandelt

--- app/src/main/java/com/example/mobappproject/activities/IngredientList.kt
+++ app/src/main/java/com/example/mobappproject/activities/IngredientList.kt
@@ -10,60 +10,57 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobappproject.R
 import com.example.mobappproject.dataClasses.Ingredient
+import com.example.mobappproject.database.DBIngredient
 import com.example.mobappproject.database.DatabaseHandler
+import com.example.mobappproject.recycleViewIngredients.RecyclerAdapter
 import com.example.mobappproject.recycleViewIngredients.RecyclerAdapterTest
+import com.example.mobappproject.recycleViewIngredients.SwipeCallback
 
 class IngredientList : AppCompatActivity() {
+
     private var linearLayoutManager: LinearLayoutManager? = null
     var recyclerView: RecyclerView? = null
     val db = DatabaseHandler(this)
-
-    private val mIngredients = arrayListOf(
-        Ingredient("Gurke"),
-        Ingredient("Tomate"),
-        Ingredient("Käse"),
-        Ingredient("Hefe"),
-        Ingredient("Mehl"),
-        Ingredient("Butter"),
-        Ingredient("Öl"),
-        Ingredient("Milch"),
-        Ingredient("Schinken"),
-    )
+    private val mIngredients = ArrayList<Ingredient>()
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingredient_list)
-        linearLayoutManager = LinearLayoutManager(this)
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView?.layoutManager = linearLayoutManager
-        recyclerView?.adapter = RecyclerAdapterTest(db.getIngredients()) //mIngredients
 
+        loadIngredients()
+
+        setRecycler()
+
+        setUpInput()
+    }
+
+    private fun setUpInput() {
         val addInput = findViewById<Button>(R.id.addInput)
         addInput.setOnClickListener {
-            newAddIng()
-           // addIngredient()
+            addIngredient()
         }
 
         val input = findViewById<EditText>(R.id.input)
-        input.setOnEditorActionListener(TextView.OnEditorActionListener  { view, keyCode, event ->
+        input.setOnEditorActionListener(TextView.OnEditorActionListener { view, keyCode, event ->
             var handled = false
-            if (keyCode == EditorInfo.IME_ACTION_DONE ) {
+            if (keyCode == EditorInfo.IME_ACTION_DONE) {
                 addIngredient()
                 handled = true
             } else if (event?.keyCode == KeyEvent.KEYCODE_ENTER && event?.action == KeyEvent.ACTION_DOWN) {
                 addIngredient()
                 handled = true
             }
-            if(handled){
+            if (handled) {
                 val focus = this.currentFocus
                 if (focus != null) {
-                    val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(focus.windowToken, 0)
                 }
             }
@@ -71,28 +68,22 @@ class IngredientList : AppCompatActivity() {
         })
     }
 
-    private fun addIngredient() {
+    private fun setRecycler() {
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView?.layoutManager = linearLayoutManager
+        val adapter = RecyclerAdapter(mIngredients)
+        recyclerView?.adapter = adapter
+        val itemTouch = ItemTouchHelper(SwipeCallback(adapter))
+        itemTouch.attachToRecyclerView(recyclerView)
+    }
+
+    private fun addIngredient(){
         val input = findViewById<EditText>(R.id.input)
         val text = input.text.toString()
         if(text != "") {
             if(!checkDoubles(text)) {
                 mIngredients.add(Ingredient(text))
-                recyclerView?.adapter?.notifyDataSetChanged()
-                recyclerView?.scrollToPosition(mIngredients.size - 1)
-                input.text.clear()
-            } else {
-                val msg = Toast.makeText(this, text + " ist bereits in der Liste", Toast.LENGTH_SHORT)
-                msg.show()
-            }
-        }
-
-    }
-
-    private fun newAddIng(){
-        val input = findViewById<EditText>(R.id.input)
-        val text = input.text.toString()
-        if(text != "") {
-            if(!checkDoubles(text)) {
                 db.addIngredient(text)
                 recyclerView?.adapter?.notifyDataSetChanged()
                 recyclerView?.scrollToPosition(mIngredients.size - 1)
@@ -108,6 +99,16 @@ class IngredientList : AppCompatActivity() {
 
     private fun checkDoubles(toAdd: String): Boolean {
         return mIngredients.contains(Ingredient(toAdd))
+    }
+
+    private fun loadIngredients() {
+        val dbIngs = db.getIngredients()
+        for (item in dbIngs){
+            val ingredient = Ingredient(item.name)
+            if(!mIngredients.contains(ingredient)) {
+                mIngredients.add(ingredient)
+            }
+        }
     }
 
 }
